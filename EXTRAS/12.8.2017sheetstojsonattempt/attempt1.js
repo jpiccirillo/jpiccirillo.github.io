1dce7613997f3ec2c8deceab9e367f722c1eae12
jpiccirillo <piccirilloj1@gmail.com>
2017-12-09 02:43:17 -0600

    adding month bucketing and month button

--- EXTRAS/12.8.2017sheetstojsonattempt/attempt1.js
+++ EXTRAS/12.8.2017sheetstojsonattempt/attempt1.js
@@ -2,31 +2,28 @@ var dates = ['x'],
     distance = ['Distance'],
     rows = new Array();
 
-bin_menu = d3.select("#bin").on("change", change_bin),
+    function makeURL() {
+        var base = 'http://gsx2json.com/api?id=',
+            key = '1flnZoRPq17SryqYN3xWC4AI4402DbO11_XHAF0c7Soo',
+            integers = 'false',
+            query = '2017-12-08',
+            rows = 'false'
 
-function makeURL() {
-    var base = 'http://gsx2json.com/api?id=',
-        key = '1flnZoRPq17SryqYN3xWC4AI4402DbO11_XHAF0c7Soo',
-        integers = 'false',
-        query = '2017-12-08',
-        rows = 'false'
-
-    url = base.concat(key, "&integers=", integers, "&rows=", rows);
-    console.log("Loading from: ", url)
-}
+        url = base.concat(key, "&integers=", integers, "&rows=", rows);
+        console.log("Loading from: ", url)
+    }
 
 function loadCSV() {
     //replace with 'url' when you're ready to call the data using a url again
     $.getJSON('fitbit.json', function(jsonData) {
             rows = jsonData.rows;
-            // console.log
         })
         .done(function() {
-            makePlot("Day");
+            makePlot("Month");
         });
 }
 
-Date.prototype.getWeek = function () {
+Date.prototype.getWeek = function() {
     var date = new Date(this.getTime());
     date.setHours(0, 0, 0, 0);
     // Thursday in current week decides the year.
@@ -48,17 +45,48 @@ function getDateOfISOWeek(w, y) {
     return ISOweekStart;
 };
 
-function bin(bucket) {
-    // console.log(columns2)
+function JFP_GetMonth(date) {
+    date = new Date(date)
+    var month_number = date.getMonth(),
+        year = date.getFullYear(),
+        month = new Date(year, month_number, 1)
+    // console.log(month)
+    return month
+}
 
+function convertDate(d) {
+    var parts = d.toString().split(" "),
+        months = {
+            Jan: "01",
+            Feb: "02",
+            Mar: "03",
+            Apr: "04",
+            May: "05",
+            Jun: "06",
+            Jul: "07",
+            Aug: "08",
+            Sep: "09",
+            Oct: "10",
+            Nov: "11",
+            Dec: "12"
+        };
+    return months[parts[1]] + "/" + parts[2] + "/" + parts[3].slice(-2);
+};
+
+function bin(bucket) {
     var nestedData2 = d3.nest()
         .key(function(d) {
-            if (bucket=="Day") {return d.date;}
-            else {return getDateOfISOWeek(new Date(d.date).getWeek(), new Date(d.date).getYear()+1900)}
+            if (bucket == "Day") {
+                return d.date;
+            } else if (bucket == "Month") {
+                return JFP_GetMonth(d.date);
+            } else {
+                return getDateOfISOWeek(new Date(d.date).getWeek(), new Date(d.date).getYear() + 1900)
+            }
         })
         .rollup(function(d) {
             return d3.sum(d, function(g) {
-                return g.distance*0.621371;
+                return g.distance * 0.621371;
             });
         }).entries(rows);
     // console.log(nestedData2)
@@ -70,10 +98,10 @@ function makePlot(bucket) {
     // console.log(bin());
 
     makeArrays(bin(bucket), bucket);
-        console.log(dates, distance);
+    // console.log(dates, distance);
     var chart = c3.generate({
         data: {
-            type: bucket=="Week" ? 'area' : 'area',
+            type: (bucket == "Day"||bucket=="Week") ? 'area' : 'area-spline',
             x: 'x',
             columns: [
                 distance,
@@ -82,52 +110,58 @@ function makePlot(bucket) {
         },
         tooltip: {
             format: {
-                value: function (value) {
+                value: function(value) {
                     var format = d3.format(",.2f");
                     return format(value) + ' mi';
                 },
             }
         },
         axis: {
-        x: {
-            type: 'timeseries',
-            extent: bucket == "Week" ? [dates[24], dates[1]] : [dates[180], dates[1]],
-            tick: {
-                fit: false,
-                // culling: false,
-                format: '%m/%d/%y',
-                // culling: {
-                //     max: window.innerWidth > 830 ? 30 : window.innerWidth >= 600 ? 14 : window.innerWidth < 500 ? 7 : 5
-                // }
-                // count: bucket=="Week"?dates.length:Math.floor(dates.length/15)
-            }
-        },
-        y: {
-            tick: {
-                format: function(x) {
-                    return x + "mi"
+            x: {
+                type: 'timeseries',
+                extent: bucket == "Week" ? [dates[24], dates[1]] :
+                (bucket == "Day" ? [dates[180], dates[1]] :
+                [dates[6], dates[1]]),
+                tick: {
+                    fit: false,
+                    // culling: false,
+                    format: '%m/%d/%y',
+                    // culling: {
+                    //     max: window.innerWidth > 830 ? 30 : window.innerWidth >= 600 ? 14 : window.innerWidth < 500 ? 7 : 5
+                    // }
+                    // count: bucket=="Week"?dates.length:Math.floor(dates.length/15)
                 }
+            },
+            y: {
+                tick: {
+                    format: function(x) {
+                        return x + "mi"
+                    }
 
+                },
             },
         },
-    },
-    bar: {
-        width: {
-            // ratio: bucket=="Week" ? 1 : .4,
-        }
-    },
-    // onresized: function() {
-    //     window.innerWidth > 830 ?
-    //         chart.internal.config.axis_x_tick_culling_max = 36 : (window.innerWidth >= 600 ? chart.internal.config.axis_x_tick_culling_max = 14 : (window.innerWidth < 500 ? chart.internal.config.axis_x_tick_culling_max = 7 : (
-    //             chart.internal.config.axis_x_tick_culling_max = 5)));
-    // },
-        subchart: { show: true },
-        point: { show: false },
-        legend: { position: 'inset' },
-        zoom: { rescale: true },
+        // onresized: function() {
+        //     window.innerWidth > 830 ?
+        //         chart.internal.config.axis_x_tick_culling_max = 36 : (window.innerWidth >= 600 ? chart.internal.config.axis_x_tick_culling_max = 14 : (window.innerWidth < 500 ? chart.internal.config.axis_x_tick_culling_max = 7 : (
+        //             chart.internal.config.axis_x_tick_culling_max = 5)));
+        // },
+        subchart: {
+            show: true
+        },
+        point: {
+            show: false
+        },
+        legend: {
+            position: 'inset'
+        },
+        zoom: {
+            rescale: true
+        },
     });
 
 }
+
 function change_bin(bucket) {
     // console.log(rows)
     //For the length of that time bucket (# categories), re-bucket data with "1" flag to
@@ -144,30 +178,28 @@ function change_bin(bucket) {
 
 function makeArrays(nestedData2, bucket) {
     // console.log(columns);
-    dates=['x']
-    distance=['Distance']
+    dates = ['x']
+    distance = ['Distance']
     console.log(nestedData2);
     for (var i = 0, len = nestedData2.length; i < len; i++) {
         // console.log(new Date(nestedData2[i].key))
         // console.log(new Date())
         // console.log(new Date(nestedData2[i].key) < new Date())
         oldDate = new Date(nestedData2[i].key),
-        today = new Date();
-        if ((oldDate < today) && (nestedData2[i].values!=0)) {
+            today = new Date();
+        if ((oldDate < today) && (nestedData2[i].values != 0)) {
             dates.push(oldDate);
-            if (bucket=="Week"){ distance.push(nestedData2[i].values/7); }
-            else {distance.push(nestedData2[i].values);}
+            if (bucket == "Day") {
+                distance.push(nestedData2[i].values);
+            } else if (bucket == "Week") {
+                distance.push(nestedData2[i].values / 7);
+            } else if (bucket == "Month") {
+                distance.push(nestedData2[i].values / 30);
+            }
         }
-        // }
-
-        // console.log(distance)
-
-
-        // cnsole.log(dates)
-        // console.log(dates);
-        // console.log(distance);
 
     }
+    console.log(dates, distance);
 
 }
 
