8aeb4f5cd71724f5d858430e8151d8230fb5d88c
jpiccirillo <piccirilloj1@gmail.com>
2018-01-14 22:13:06 -0600

    slider dimensions flexible based on overall size of tool, rather than px hardcoding

--- PowerApplet/dragrect_second.js
+++ PowerApplet/dragrect_second.js
@@ -15,6 +15,22 @@ $(function() {
 });
 
 $(function() {
+    $("#slider-vertical1_shadow").slider({
+        orientation: "vertical",
+        range: "min",
+        min: 0,
+        max: 100,
+        value: 10,
+        step: 5,
+
+        slide: function(event, ui) {
+            console.log(ui.value)
+        }
+    });
+    $(".ui-slider-range-min").css("background-color", "lightblue");
+});
+
+$(function() {
     $("#slider-vertical2").slider({
         orientation: "vertical",
         range: "min",
