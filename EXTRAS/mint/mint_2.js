d593d40701a57be687e4c30a35cc2012814c583e
jpiccirillo <piccirilloj1@gmail.com>
2018-10-11 22:19:00 -0500

    first stab at cleaning up the mint miniapplication, using better js constructs like map/filter/reduce, at very least .forEach().  Good enough but to really improve i'll probably have to do a more OOP-type overhaul

--- EXTRAS/mint/mint_2.js
+++ EXTRAS/mint/mint_2.js
@@ -7,7 +7,8 @@ var brush = [],
     parseDate = d3.time.format("%m/%d/%Y").parse,
     formatCount = d3.format(",.2f"),
     monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    choicesarray = []
+    choicesarray = [],
+    csv_data = [];
 
 for (i in binArray) {
     finalDataTotal.push([binArray[i]])
@@ -25,45 +26,31 @@ d3.queue()
     .await(process_csvs);
 
 function process_csvs(error, csv_data, early_csv_data) {
-    csv_data.forEach(function (d) {
-        d.Amount = +d.Amount
-        d.Date = parseDate(d["Date"])
-        console.log(d.Date)
-        d.Week = getDateOfISOWeek(d.Date.getWeek(), (d.Date.getYear() + 1900).toString());
-        d.Month = JFP_GetMonth(d.Date);
-        d.Category = d.Category;
-    });
-    early_csv_data.forEach(function (d2) {
-        d2.Amount = +d2.Amount
-        d2.Date = parseDate(d2["Date"])
-        d2.Week = getDateOfISOWeek(d2.Date.getWeek(), (d2.Date.getYear() + 1900).toString());
-        d2.Month = JFP_GetMonth(d2.Date);
-        d2.Category = d2.Category;
-    });
-    csv_data = csv_data.concat(early_csv_data)
 
+    function convert(csv) {
+        csv.forEach(function (d) {
+            d.Amount = +d.Amount
+            d.Date = parseDate(d["Date"])
+            d.Week = getDateOfISOWeek(d.Date.getWeek(), (d.Date.getYear() + 1900).toString());
+            d.Month = JFP_GetMonth(d.Date);
+            d.Category = d.Category;
+        });
+        return csv;
+    }
+
+    csv_data = convert(csv_data).concat(convert(early_csv_data));
+    console.log(csv_data)
     window.csv_data = csv_data
 
-    for (var i = 0, len = csv_data.length; i < len; i++) {
-        if (csv_data[i]["Transaction Type"] == 'debit') {
-            csv_data[i].Multiplier = -1
-        } else csv_data[i].Multiplier = 1;
+    csv_data.forEach(function(val, index) {
+        val.Multiplier = (val["Transaction Type"] == 'debit') ? -1 : 1;
+        val.Multiplier = ((val.Category == 'Transfer') && (val.Amount > 10)) ? 0 : val.Multiplier;
+        val.Multiplier = (val.Original == 'Credit Card Payment') ? 0 : val.Multiplier
+        val.Multiplier = (val["Original Description"].indexOf("SRVC DES:AUTO") != -1) ? 0 : val.Multiplier
 
-        if (((csv_data[i].Category == 'Transfer') && (csv_data[i].Amount > 10))
-            || (csv_data[i].Original == 'Credit Card Payment')
-            || (csv_data[i]["Original Description"].indexOf("SRVC DES:AUTO") != -1)) {
-            csv_data[i].Multiplier = 0 }
-    };
-
-    for (var i = 0, len = csv_data.length; i < len; i++) {
-        //        console.log(csv_data[i].Date);
-        normaldate = csv_data[i].Date
-        to_miliseconds = Date.parse(normaldate);
-        csv_data[i].DateMS = to_miliseconds
-        var timestamp = new Date(csv_data[i].DateMS)
-        csv_data[i].Date = timestamp;
-        //        console.log(csv_data[i].Date);
-    };
+        val.DateMS = Date.parse(val.Date);
+        val.Date = new Date(val.DateMS);
+    })
 
     var s = new Date(Math.min.apply(Math, csv_data.map(function (o) { return o.DateMS; }))),
         e = new Date(Math.max.apply(Math, csv_data.map(function (o) { return o.DateMS; }))),
@@ -102,34 +89,36 @@ function process_csvs(error, csv_data, early_csv_data) {
     bucketData(null, 0)
 };
 
-//  1) - detect which category and time series are selected currently, store in local variable
-//  2) - bucket time series first to generate a time-array of summed days (or weeks / months)
-//  3) - go through csv_data array and cherry pick values and put them in an intermediary array maybe?
+/*
+1) - detect which category and time series are selected currently, store in local variable
+2) - bucket time series first to generate a time-array of summed days (or weeks / months)
+3) - go through csv_data array and cherry pick values and put them in an intermediary array maybe?
 
-//  The purpose of this function is to take the current user selection (category and time-division),
-//  bucket the correct data from csv_data, and then add that bucketed data to finalDataDays[],
-//  finalDataWeeks[], or finalDataMonths[].
+The purpose of this function is to take the current user selection (category and time-division),
+bucket the correct data from csv_data, and then add that bucketed data to finalDataDays[],
+finalDataWeeks[], or finalDataMonths[].
+*/
 
 function bucketData(category, replot) {
     var bucket = bin_menu.property("value");
-        console.log(bin_menu)
-    if (replot == 0) {category = menu.property("value")}
+    // console.log(bin_menu)
+    if (replot == 0) { category = menu.property("value") }
 
     //If switching bucket and categories already exist in other bucket, don't reparse everything
     if ((replot == 1) && checkDuplicates(category, bucket)) {return;}
 
     filled_days = [];
-    for (var i = 0, len = csv_data.length; i < len; i++) {
-        if (csv_data[i].Category == category) {
+    csv_data.forEach(function(val, index) {
+        if (val.Category == category) {
             filled_days.push({
-                Signed_Amount: csv_data[i].Amount * csv_data[i].Multiplier,
-                Date: convertDate(csv_data[i].Date),
-                Category: csv_data[i].Category,
-                Week: convertDate(csv_data[i].Week),
-                Month: csv_data[i].Month
+                Signed_Amount: val.Amount * val.Multiplier,
+                Date: convertDate(val.Date),
+                Category: val.Category,
+                Week: convertDate(val.Week),
+                Month: val.Month
             })
         }
-    };
+    })
 
     combined_unbinned_original = filled_days.concat(all_days);
     // console.log(combined_unbinned_original)
@@ -140,7 +129,7 @@ function bucketData(category, replot) {
 }
 
 function bin_data2(combined_unbinned, bucket, category) {
-    console.log(bucket)
+    // console.log(bucket)
     nestedData2 = d3.nest()
         .key(function (d) {
             return d[bucket];
@@ -152,7 +141,7 @@ function bin_data2(combined_unbinned, bucket, category) {
         })
         .entries(combined_unbinned);
 
-    console.log(nestedData2);
+    // console.log(nestedData2);
 
     for (i in finalDatesTotal) {
         // if the correct date array has only one entry (the initial label)
@@ -163,7 +152,7 @@ function bin_data2(combined_unbinned, bucket, category) {
             }
         }
     }
-    console.log(finalDatesTotal)
+    // console.log(finalDatesTotal)
     // Sort Amount Array and Date arrays
     sorting(nestedData2, 'key');
     for (i in finalDatesTotal) {
@@ -235,10 +224,14 @@ function modernDrawPlot(bucket,rezoom) {
 //        [last_month_extent + day * 7 - zoomdepth, last_month_extent + day * 7];
 //        [last_month_extent + day * 2 - zoomdepth, last_month_extent + day * 2];]
 
-//We want it to enforce the special right-side brush formatting when:
-//    - brush.length = 0 aka the plot is loading for the first time
-//    - rezoom == 1 aka the "zoom to last X months" has been selected
-//    - the current brush is in the next month as current (this has to go last in the conditions otherwise will be null pointer if page is first loading (since brush             doesn't exist yet).  This is for the condition where user rebins without changing the brush.
+
+/*
+We want it to enforce the special right-side brush formatting when:
+    - brush.length = 0 aka the plot is loading for the first time
+    - rezoom == 1 aka the "zoom to last X months" has been selected
+    - the current brush is in the next month as current (this has to go last in the conditions otherwise will be null pointer if page is first loading (since brush doesn't exist yet).
+    This is for the condition where user rebins without changing the brush.
+*/
 
     var firstDate = new Date(brush[1]).getTime();
     var secondDate = new Date().getTime();
@@ -320,32 +313,6 @@ function modernDrawPlot(bucket,rezoom) {
     });
 }
 
-//function reloadChart(bucket) {
-//    //    console.log("last domain of brush: ", brush);
-//    var arrayToPlot = [],
-//        datesToPlot = ['x'],
-//        plottingArray = [];
-//
-//    for (i in finalDataTotal) {
-//        if (finalDataTotal[i][0] == bucket) {
-//            for (var j = 1, len = finalDataTotal[i].length; j < len; j++) {
-//                plottingArray.push(finalDataTotal[i][j]);
-//            }
-//            for (var j = 1, len = finalDatesTotal[i].length; j < len; j++) {
-//                datesToPlot.push(finalDatesTotal[i][j]);
-//            }
-//        }
-//    }
-//
-//    plottingArray.push(datesToPlot);
-//    setTimeout(function () {
-//        bindto: "#chart"
-//        this.chart.load({
-//            columns: plottingArray
-//        }, );
-//    }, -1);
-//}
-
 function checkDuplicates(category,bucket) {
     for (i in finalDataTotal)
         if (finalDataTotal[i][0] == bucket) {
@@ -371,7 +338,7 @@ function ClearPlot() {
 
     //call replot so that c3.js can plot modified/reduced array using
     //currently selected bucket
-    console.log(finalDataTotal);
+    // console.log(finalDataTotal);
     modernDrawPlot(bin_menu.property("value"));
 };
 
@@ -386,17 +353,22 @@ function change_category() {
 };
 
 function change_bin() {
-    var longest=0;
+    longest=0;
 
-    //Find the time bucket that has the most categories added to it so far
-    for (i in finalDataTotal) {
-        if (finalDataTotal[i].length >= finalDataTotal[longest].length) { longest = i }
-    }
+    /*Find the time bucket that has the most categories added to it so far.
+    We care about this because after switching from day->month, the user will expect to see all catagories that they had been addding previously.
+    So we need to find the time bucket with the most categories so when rebucketing, we can pull all those through
 
-    //For the length of that time bucket (# categories), re-bucket data with "1" flag to
-    //manually specifiy categories instead of pulling from current selection
-    for (var i=1, len=finalDataTotal[longest].length; i<len; i++) {
-            bucketData(finalDataTotal[longest][i][0],1)
+    I've tried to clean up this code and use map/filter/reduce but honestly has been gnarly
+    */
+    finalDataTotal.forEach(function(val, i) {
+        if (val.length>=finalDataTotal[longest].length) { longest= +i; }
+    })
+
+    //For the length of that time bucket (# categories), re-bucket data with "1" flag to manually specifiy categories instead of pulling from current selection
+    var lArray = finalDataTotal[longest]
+    for (var i=1, len=lArray.length; i<len; i++) {
+            bucketData(lArray[i][0],1)
     }
 
     //Finally re-plot using current time bucket selection
