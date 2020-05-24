e5b11c3f41fb5e3ae764594bc9bed8b9f9727cee
Jeffrey <piccirilloj1@gmail.com>
2020-05-23 23:32:11 -0500

    Add new functionality to Curve Class
    - One instance of this class (for the time being at least) handles all three curves (the clipped/darker error background, the non-clipped curve, and the curve in the top panel)
    - More refactoring and debugging drag functionality, getting drag to work on all three curves while also getting the clipping path to clip only one of them
    - Found bug in normalcdf (mu0 was coming in as a string and thus not being added...).  I believe at the time i massaged the surrounding math to get it to return what it was supposed to, wo knowing why it wasnt coming out right

--- PowerApplet/Curve.js
+++ PowerApplet/Curve.js
@@ -1,63 +1,101 @@
 class Curve {
   constructor(options) {
     Object.assign(this, options);
-
-    this.palette = d3
-      .select(`#${this.anchor}`)
-      .append("svg")
-      .attr("width", 500)
-      .attr("height", 500);
-
+    this.top = $(".minigraph").innerHeight();
+    this.bottom = $(".maingraph").innerHeight() - 20;
     this.addPath();
-
     channel.on("change", () => this.removePath() && this.addPath());
   }
 
-  static generateCurve() {
-    const { mu1, std, n } = p;
-    const l_bound = mu1 - 4 * std;
-    const u_bound = mu1 + 4 * std;
+  generateCurve(position) {
+    let { std, n } = p;
+    const l_bound = p[this.center] - 4 * std;
+    const u_bound = p[this.center] + 4 * std;
     const array = [];
     const step = (8 * std) / 30;
+    if (position === "top") n = 1.25;
 
     for (let k = l_bound; k < u_bound; k += step) {
-      const x = screenScale(k);
-      const y = verticalScale(pdf(k, mu1, std / Math.sqrt(n)));
+      const x = screenScale({ ...p, x: k });
+      const y = verticalScale({
+        ...p,
+        y: pdf(k, p[this.center], std / Math.sqrt(n)),
+      });
       array.push({ x, y });
     }
+    this.array = array;
     return array;
   }
 
   drag() {
     let initial = 0;
+    let _this = this;
     return d3
       .drag()
       .on("start", (d) => (initial = d.x))
       .on("drag", (d) => {
         d.x += d3.event.dx;
-        const movement = `translate(${d.x},${screen_h - 20}) scale(1,-1)`;
-        d3.select(`#${this.id}`).attr("transform", movement);
+        d3.selectAll(`#${this.id},#${this.id}-error`).attr(
+          "transform",
+          `translate(${d.x},${this.bottom}) scale(1,-1)`
+        );
+        d3.selectAll(`#${this.id}top`).attr(
+          "transform",
+          `translate(${d.x},${this.top}) scale(1,-1)`
+        );
       })
       .on("end", function (d) {
-        const newMu1 = p.mu1 + ((d.x - initial) * 8 * p.std) / screen_w;
-        setValuesNew({ id: "mu1", value: newMu1 });
-        channel.emit("change", newMu1);
+        const newMu =
+          p[_this.center] + ((d.x - initial) * 8 * p.std) / screen_w;
+        setValuesNew({ id: _this.center, value: newMu });
       });
   }
 
   addPath() {
-    const path = this.palette
+    this.hasError &&
+      mainContainer
+        .append("g")
+        .attr("id", "clip-wrapper")
+        .attr("clip-path", `url(#rect-clip-${this.clip})`)
+        .append("path")
+        .attr("id", `${this.id}-error`)
+        .attr("d", interp(this.generateCurve()))
+        .attr("fill", `rgba(${this.color}, .70)`)
+        .attr("transform", `translate(0, ${this.bottom}) scale(1,-1)`);
+
+    const path = mainContainer
       .append("path")
       .attr("id", this.id)
-      .attr("d", interp(Curve.generateCurve()))
-      .attr("transform", "translate(0," + (screen_h - 20) + ") scale(1,-1)");
+      .attr("d", interp(this.generateCurve()))
+      .attr("fill", `rgba(${this.color}, .60)`)
+      .attr("stroke", `rgba(${this.color}, 1)`)
+      .attr("stroke-width", "1.5px")
+      .attr("transform", `translate(0, ${this.bottom}) scale(1,-1)`);
 
-    if (this.draggable) {
-      path.data([{ x: 0 }]).call(this.drag());
-    }
+    topContainer
+      .append("path")
+      .attr("id", `${this.id}top`)
+      .attr("d", interp(this.generateCurve("top")))
+      .attr("fill", "none")
+      .attr("stroke", `rgba(${this.color}, 1)`)
+      .attr("stroke-width", "2px")
+      .attr("transform", `translate(0, ${this.top}) scale(1,-1)`);
+
+    if (this.draggable) path.data([{ x: 0 }]).call(this.drag());
   }
 
   removePath() {
-    return $(`#${this.id}`).remove().length;
+    $(`#${this.id},#${this.id}-error`).remove();
+    $(`#${this.id}top`).remove();
+    return true;
+  }
+
+  addText() {
+    topContainer
+      .append("text")
+      .attr("id", this.id + "text")
+      .attr("x", -100)
+      .attr("y", -200)
+      .text(this.text);
   }
 }
