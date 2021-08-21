e1a256942a979773ed6bdd64e60e870b7de103d4
jeffrey <piccirilloj1@gmail.com>
2021-08-21 14:46:22 -0500

    Download Open Sans font into repository for less flicker/unloaded-content effect (FOUC)

--- new_section/src/main.js
+++ new_section/src/main.js
@@ -5,6 +5,7 @@ import VueFeatherIcon from "vue-feather-icon";
 import "@/assets/scss/app.scss";
 import "@/assets/scss/newSidebar.scss";
 import "@/assets/scss/voronoi.scss";
+import "@/assets/scss/fonts.scss";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
