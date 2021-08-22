d7f65c8bdd1c637bfdf025a02a3ad65f9ec8a8ef
jeffrey <piccirilloj1@gmail.com>
2021-08-22 16:45:33 -0500

    Finally iron out clean / non glitchy first run through of sidebar
    - Move click handlers into the template and use native vue methods
    - Remove hover events for greater consistency across devices

--- new_section/src/App.vue
+++ new_section/src/App.vue
@@ -1,82 +1,110 @@
 <template>
   <div id="app">
     <div class="sidebar">
-      <ul class="ul">
-        <li class="li item active">
+      <ul
+        class="ul"
+        :class="sidebarClass"
+        @click.capture="expandSidebar($event)"
+      >
+        <li
+          class="li item"
+          @click.stop="setSwoop('home')"
+          :class="isSwoop('home')"
+        >
           <i data-feather="home"></i><span class="text">Home</span>
         </li>
-        <li class="li item">
+        <li
+          class="li item"
+          @click.stop="setSwoop('font')"
+          :class="isSwoop('font')"
+        >
           <i data-feather="framer"></i><span class="text">Font</span>
         </li>
-        <li class="li title">
+        <li
+          class="li title"
+          :class="`${isSwoop('map')} ${isActiveDropdown('map')}`"
+          @click="showDropdown('map')"
+        >
           <div class="title-header">
             <i data-feather="map"></i>
             <span class="text">Mapping</span>
             <i data-feather="chevron-down" class="expand-icon"></i>
           </div>
           <ul id="inner-wrapper">
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('map')">
               <span class="text">Geological</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('map')">
               <span class="text">Medieval Spain</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('map')">
               <span class="text">Political / Choropleth</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('map')">
               <span class="text">Campus Walking Map</span>
             </li>
           </ul>
         </li>
-        <li class="li title">
+        <li
+          class="li title"
+          :class="`${isSwoop('p')} ${isActiveDropdown('p')}`"
+          @click="showDropdown('p')"
+        >
           <div class="title-header">
             <i data-feather="monitor"></i>
             <span class="text">Programming</span>
             <i data-feather="chevron-down" class="expand-icon"></i>
           </div>
           <ul id="inner-wrapper">
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('p')">
               <span class="text">D3 Interactive Map</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('p')">
               <span class="text">Statistical Power Applet</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('p')">
               <span class="text">Fitbit Data Visualization</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('p')">
               <span class="text">Mint Financial Data</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('p')">
               <span class="text">Computer Productivity</span>
             </li>
           </ul>
         </li>
-        <li class="li title">
+        <li
+          class="li title"
+          :class="`${isSwoop('photo')} ${isActiveDropdown('photo')}`"
+          @click="showDropdown('photo')"
+        >
           <div class="title-header">
             <i data-feather="camera"></i>
             <span class="text">Photography</span>
             <i data-feather="chevron-down" class="expand-icon"></i>
           </div>
           <ul id="inner-wrapper">
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('photo')">
               <span class="text">Places</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('photo')">
               <span class="text">People</span>
             </li>
-            <li class="li item">
+            <li class="li item" @click.stop="setSwoop('photo')">
               <span class="text">Surf</span>
             </li>
           </ul>
         </li>
-        <li class="li item">
+        <li
+          class="li item"
+          @click.stop="setSwoop('abt')"
+          :class="isSwoop('abt')"
+        >
           <i data-feather="smile"></i><span class="text">About</span>
         </li>
       </ul>
     </div>
-    <div id="portfolio">
+    <div id="portfolio" @click="contractSidebar">
       <div id="entry" />
     </div>
   </div>
@@ -87,60 +115,44 @@ import feather from "feather-icons";
 
 export default {
   name: "App",
+  data() {
+    return {
+      activeItem: "none",
+      activeDropdown: "none",
+      sidebarClass: "",
+    };
+  },
   mounted() {
-    this.onResize();
     feather.replace();
-    window.addEventListener("resize", this.onResize);
-    let items = document.querySelectorAll(".item");
-
-    items.forEach((item) => {
-      item.addEventListener("click", () => {
-        items.forEach((item) => item.classList.remove("active"));
-        item.classList.add("active");
-      });
-    });
-
-    let titles = document.querySelectorAll(".title");
-    let sidebar = document.querySelectorAll(".ul")[0];
-    sidebar.addEventListener("click", () => {
-      sidebar.classList.add("clicked");
-    });
-
-    let mainApp = document.getElementById("portfolio");
-    let entries = document.querySelectorAll(".item");
-    mainApp.addEventListener("click", () => {
-      sidebar.classList.remove("clicked");
-      titles.forEach((item) => item.classList.remove("clicked"));
-    });
-    entries.forEach((e) => {
-      e.addEventListener("click", () => {
-        sidebar.classList.remove("clicked");
-        titles.forEach((item) => item.classList.remove("clicked"));
-      });
-    });
-
-    titles.forEach((e) => {
-      e.addEventListener("click", () => {
-        titles.forEach((item) => item.classList.remove("clicked"));
-        e.classList.add("clicked");
-      });
-    });
   },
   methods: {
-    handleClick() {},
+    setSwoop(name) {
+      this.activeItem = name;
+      this.sidebarClass = ""; // Contract sidebar when sthing's clicked
+      this.activeDropdown = ""; // Close all dropdowns
+    },
+    isSwoop(name) {
+      return this.activeItem === name ? "active" : "";
+    },
+    showDropdown(name) {
+      this.activeDropdown = this.activeDropdown === name ? "" : name;
+    },
+    isActiveDropdown(name) {
+      return this.activeDropdown === name ? "clicked" : "";
+    },
+    expandSidebar(e) {
+      if (this.sidebarClass !== "clicked") {
+        this.sidebarClass = "clicked";
+        e.stopImmediatePropagation();
+      }
+    },
+    contractSidebar() {
+      this.sidebarClass = "";
+      this.activeDropdown = "";
+    },
     onToggleCollapse(collapsed) {
       this.collapsed = collapsed;
       this.menu[0].title = this.collapsed ? "JP" : "Jeffrey Piccirillo";
-    },
-    onItemClick() {},
-    onResize() {
-      if (window.innerWidth <= 767) {
-        this.isOnMobile = true;
-        this.collapsed = true;
-      } else {
-        this.isOnMobile = false;
-        this.collapsed = false;
-      }
     },
   },
 };
