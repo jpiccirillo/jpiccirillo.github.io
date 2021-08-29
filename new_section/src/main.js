a3e1eec1934d27ad03e393147f42c35d8cb50ea4
jeffrey <piccirilloj1@gmail.com>
2021-08-29 00:02:18 -0500

    Install google analytics

--- new_section/src/main.js
+++ new_section/src/main.js
@@ -7,14 +7,21 @@ import "@/assets/scss/voronoi.scss";
 import "@/assets/scss/fonts.scss";
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import router from './router'
+import router from "./router";
+import VueAnalytics from "vue-analytics";
 
 Vue.use(VueFeatherIcon);
 Vue.use(VueSidebarMenu);
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+  id: "UA-96097643-1",
+  router,
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
