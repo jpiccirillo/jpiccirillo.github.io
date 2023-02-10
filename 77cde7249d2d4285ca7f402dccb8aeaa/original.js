18fd6f476b59e0a14aa157c8badfe911d21f5d3a
jpiccirillo <piccirilloj1@gmail.com>
2023-02-10 12:08:41 -0600

    Revert "Portoflio page development in new_section directory (#6)"
    This reverts commit bdf9d4d57809d0b059cb77058bf829ee45b4bbd4.

---
+++ 77cde7249d2d4285ca7f402dccb8aeaa/original.js
@@ -0,0 +1,59 @@
+const first = {
+  ual: {
+    shares: 88,
+    basis: 59.98,
+  },
+  dal: {
+    shares: 98,
+    basis: 50.69,
+  },
+  aal: {
+    shares: 208,
+    basis: 24.04,
+  },
+  ccl: {
+    shares: 171,
+    basis: 29.11,
+  },
+  aapl: {
+    shares: 40,
+    basis: 127.24,
+  },
+  rblx: {
+    shares: 71,
+    basis: 69.89,
+  },
+  nclh: {
+    shares: 158,
+    basis: 31.57,
+  },
+  rcl: {
+    shares: 54,
+    basis: 91.88,
+  },
+};
+
+const second = {
+  ual: {
+    shares: 39,
+    basis: 50.53,
+  },
+  dal: {
+    shares: 46,
+    basis: 42.83,
+  },
+  aal: {
+    shares: 95,
+    basis: 20.86,
+  },
+  ccl: {
+    shares: 86,
+    basis: 23.87,
+  },
+  nclh: {
+    shares: 74,
+    basis: 27.22,
+  },
+};
+
+const allPurchases = { first, second };
